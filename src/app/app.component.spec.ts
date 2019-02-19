daa3b5a6b61ac93885556a0ef2c96f1ed107bf6d
Adam Kariv <adam.kariv@gmail.com>
2019-02-20 00:30:56 +0200

    initial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'reportit-user'`, () => {
+  it(`should have as title 'reportit-agent'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('reportit-user');
+    expect(app.title).toEqual('reportit-agent');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to reportit-user!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to reportit-agent!');
   });
 });
