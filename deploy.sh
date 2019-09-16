3628acf5e629ab6b50e2df18764a65a34f20d68a
Adam Kariv <adam.kariv@gmail.com>
2019-09-16 21:52:11 +0300

    rm dist

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,11 @@ git checkout master && \
 (git branch -D dist || true) && \
 git checkout -b dist && \
 rm .gitignore && \
-ng build --prod && \
+ng build --prod -c production && \
+ng build --prod -c ar && \
+ng build --prod -c en && \
+ng build --prod -c ru && \
+ng build --prod -c am && \
 cp CNAME dist/reportit-user/ && \
 git add dist/reportit-user && \
 git commit -m dist && \
